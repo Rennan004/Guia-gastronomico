52df42ac60d733ed0d8f304f2af51ed9c2b2151e
Rennan004 <rennan.guilherme63@gmail.com>
2022-06-07 11:46:01 -0300

    a

--- src/conexao/conn.php
+++ src/conexao/conn.php
@@ -2,7 +2,7 @@
 
     // Declarar as variáveis necessárias para a conexão com o banco de dados........
     $hostname = "sql103.epizy.com"; //Nome do servidor que se encontra nosso banco de dados
-    $dbname = "epiz_31505642_BancoTCC"; //Nome do nosso banco de dados
+    $dbname = "epiz_31505642_BANCO_TCC"; //Nome do nosso banco de dados
     $username = "epiz_31505642"; //Nome do usuário para acesso ao banco de daddos
     $password = "1159626"; //Senha de acesso ao nosso banco de daddos
 
