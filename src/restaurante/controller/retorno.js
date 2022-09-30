0f4dcdd427c17958ed68ba0d891c7ddeefecfaec
Rennan004 <rennan.guilherme63@gmail.com>
2022-09-30 12:54:10 -0300

    a

--- src/restaurante/controller/retorno.js
+++ src/restaurante/controller/retorno.js
@@ -1,8 +1,7 @@
 $(document).ready(function() {
-    $('.btn-send').click(function(e) {
+    $('.teste').click(function(e) {
         e.preventDefault()
 
-        let dados = $('#form').serialize()
 
         $('#retorno').empty()
 
@@ -12,18 +11,18 @@ $(document).ready(function() {
             assync: true,
             data: dados,
             url: '../modelo/retorno.php',
-            success: function(dados) {
+            success: function(dado) {
                 $('#retorno').append(`
                 <div class="col-lg-4 templatemo-item-col all soon">
                 <div class="meeting-item">
                     <div class="thumb">
-                        <a href="local.html" class="cardzinho"><img src="../model/fotos/${dados.tipo}" alt=""></a>
+                        <a href="local.html" class="cardzinho"><img src="../model/fotos/${dado.dados.FOTO}" alt=""></a>
                     </div>
                     <div class="down-content">
                         <a href="local.html" class="cardzinho">
-                            <h4>${dados.tipo}</h4>
+                            <h4>${dado.dados.NOME}</h4>
                         </a>
-                        <p>${dados.tipo}.</p>
+                        <p>${dado.dados.DESCRICAO}.</p>
                     </div>
                 </div>
             </div>`)
