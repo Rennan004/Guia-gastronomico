c92b4e61c9eefb2ed34a630d2ee59bd32f71b561
Rennan004 <rennan.guilherme63@gmail.com>
2022-11-22 10:52:07 -0300

    retorno

---
+++ src/restaurante/controller/retorno-index.js
@@ -0,0 +1,39 @@
+$(document).ready(function() {
+    // $('.teste').click(function(e) {
+    //     e.preventDefault()
+
+
+        $('#retorno').empty()
+
+        $.ajax({
+            type: 'POST',
+            dataType: 'JSON',
+            assync: true,
+            url: 'src/restaurante/model/retorno.php',
+            success: function(dados) {
+                for(const dado of dados){
+                    $('#retorno-index').append(`
+                    <div class="col-lg-6">
+                            <div class="meeting-item">
+                                
+                        
+                                    <div class="thumb">
+                                    <a href="local.html" class="cardzinho"><img src="src/restaurante/model/fotos/${dado.FOTO}" alt=""></a>
+                                </div>
+                                
+                                <div class="down-content">
+                                    <div class="date">
+
+                                    </div>
+                                    <a href="#">
+                                        <h4>${dado.NOME}</h4>
+                                    </a>
+                                    <p>${dado.DESCRICAO}</p>
+                                </div>
+                            </div>
+                        </div>`)
+                }
+            }
+        })
+    })
+// })
